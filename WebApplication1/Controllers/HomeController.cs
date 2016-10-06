17699afda8bc1df2f5c6d743bb318a52f495addc
Ebony Gordon <ebony.gordon@cognizant.com>
2016-10-06 14:35:05 -0600

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var vm = new HomeViewModel();
+            vm.UserName = Environment.UserName;
+            vm.Domain = Environment.UserDomainName;
+            return View(vm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
